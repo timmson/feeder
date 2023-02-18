03b5e7456e4482c4ee7e6b1ad4c503a89c69af54
timmson <timmson666@mail.ru>
2023-02-18 19:36:08 +0300

    Add MREDC and IMOEX

--- feeder-stock/src/main/kotlin/ru/timmson/feeder/stock/dao/RequesterImpl.kt
+++ feeder-stock/src/main/kotlin/ru/timmson/feeder/stock/dao/RequesterImpl.kt
@@ -14,9 +14,11 @@ class RequesterImpl : Requester {
 
     override fun fetch(url: String): String {
         log.info("Entering fetch ($url) ... ")
+
         val builder = HttpRequest.newBuilder(URI(url))
         val request = builder.GET().build()
         val body = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString()).body()
+
         log.info("Leaving fetch (...${url.substring(url.length - 10)}) = [length=${body.length}] ")
         return body
     }
