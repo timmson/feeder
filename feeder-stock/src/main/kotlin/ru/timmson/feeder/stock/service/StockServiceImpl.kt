f9eba43075394d7e988354134d38addd72103192
timmson <timmson666@mail.ru>
2023-02-19 16:53:51 +0300

    Add coroutines

--- feeder-stock/src/main/kotlin/ru/timmson/feeder/stock/service/StockServiceImpl.kt
+++ feeder-stock/src/main/kotlin/ru/timmson/feeder/stock/service/StockServiceImpl.kt
@@ -1,25 +1,44 @@
 package ru.timmson.feeder.stock.service
 
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 import ru.timmson.feeder.stock.dao.MarketWatchDAO
 import ru.timmson.feeder.stock.dao.MoscowExchangeDAO
+import ru.timmson.feeder.stock.dao.StockDAO
 import ru.timmson.feeder.stock.model.Stock
-import java.util.stream.Collectors
+import java.util.logging.Logger
 
 @Service
 class StockServiceImpl(
-    private val moscowExchangeDAO: MoscowExchangeDAO,
-    private val marketWatchDAO: MarketWatchDAO
+    moscowExchangeDAO: MoscowExchangeDAO,
+    marketWatchDAO: MarketWatchDAO
 ) : StockService {
 
+    private val log = Logger.getLogger(StockServiceImpl::class.toString())
+
     private val stocks = mapOf(
-        "usd" to { moscowExchangeDAO.getStockByTicker("usd") },
-        "imoex" to { moscowExchangeDAO.getStockByTicker("imoex") },
-        "mrdec" to { moscowExchangeDAO.getStockByTicker("mredc") },
-        "spx" to { marketWatchDAO.getStockByTicker("spx") },
-        "shcomp" to { marketWatchDAO.getStockByTicker("shcomp") },
+        "usd" to moscowExchangeDAO,
+        "imoex" to moscowExchangeDAO,
+        "mredc" to moscowExchangeDAO,
+        "spx" to marketWatchDAO,
+        "shcomp" to marketWatchDAO
     )
 
-    override fun findAll(): List<Stock> =
-        stocks.values.parallelStream().map { it.invoke() }.collect(Collectors.toList())
+    override fun findAll(): List<Stock> = runBlocking {
+        stocks.entries.asFlow().transform { emit(getStock(it)) }.toList()
+    }
+
+    fun getTickers() = stocks.keys
+
+    private fun getStock(it: Map.Entry<String, StockDAO>) =
+        try {
+            it.value.getStockByTicker(it.key)
+        } catch (e: Exception) {
+            log.severe("Stock ${it.key} is not received: $e")
+            Stock(it.key)
+        }
+
 }
