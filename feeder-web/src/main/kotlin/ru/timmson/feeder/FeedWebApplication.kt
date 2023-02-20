cf27da01c86d65b9b851bba915d18350dd850122
timmson <timmson666@mail.ru>
2023-02-20 23:20:15 +0300

    Add version

--- feeder-web/src/main/kotlin/ru/timmson/feeder/FeedWebApplication.kt
+++ feeder-web/src/main/kotlin/ru/timmson/feeder/FeedWebApplication.kt
@@ -1,12 +1,26 @@
 package ru.timmson.feeder
 
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.logging.Logger
 
 @EnableScheduling
 @SpringBootApplication
-open class FeedWebApplication
+open class FeedWebApplication(
+    @Value("\${feeder.version}") private val version: String
+) {
+
+    private val log = Logger.getLogger(FeedWebApplication::class.java.toString())
+
+    @PostConstruct
+    fun printInfo() {
+        log.info("${FeedWebApplication::class.simpleName} version $version")
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<FeedWebApplication>(*args)
