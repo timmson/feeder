a5ba9f1fdd49a85420c7b42bff797d6e5d9d113a
Artem Krotov <timmson666@mail.ru>
2023-12-18 11:40:07 +0300

    - Add developer_ pattern (#31)

--- feeder-web/src/main/kotlin/ru/timmson/feeder/Version.kt
+++ feeder-web/src/main/kotlin/ru/timmson/feeder/Version.kt
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service
 
 @Service
 data class Version(
-    val number: String = "26",
-    val feature: String = "Add CV_ pattern"
+    val number: String = "27",
+    val feature: String = "Add developer_ pattern"
 ) {
 
     override fun toString(): String {
