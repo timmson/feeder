22603439b612a43168c307d1daf18b1a0bd2c90b
Artem Krotov <timmson666@mail.ru>
2025-07-07 22:25:28 +0300

    Improve ai (#51)
    * Improve AI
    
    * Fix versions

--- feeder-web/src/main/kotlin/ru/timmson/feeder/service/FeederFacade.kt
+++ feeder-web/src/main/kotlin/ru/timmson/feeder/service/FeederFacade.kt
@@ -87,12 +87,12 @@ class FeederFacade(
                 url = cv.url
             )
 
-        cvStore.add(fields)
+        //cvStore.add(fields)
+        botService.sendMessage(cvRequest.chatId, "Кандидат:\n<code>$fields</code>")
 
-        /*        val cvFile = botService.downloadFile(cvRequest.fileId)
-                val estimation = cvEstimationService.estimate(cv.type, cvFile)*/
-
-        botService.sendMessage(cvRequest.chatId, "<code>$fields</code>")
+        val cvFile = botService.downloadFile(cvRequest.fileId)
+        val estimation = cvEstimationService.estimate(cv.title.lowercase(), cvFile)
+        botService.sendMessage(cvRequest.chatId, "Оценка кандидата ${cv.name} на позиицию ${cv.title.lowercase()}:\n\n<i>$estimation</i>")
 
         log.info("Leaving registerCV(...) = $cv")
     }
--- feeder-yandex/build.gradle.kts
+++ feeder-yandex/build.gradle.kts
@@ -1,7 +1,9 @@
 val apachePdfBoxVersion: String by project
+val okHttpVersion: String by project
 
 dependencies {
     implementation(project(":feeder-common"))
 
     implementation("org.apache.pdfbox:pdfbox:$apachePdfBoxVersion")
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
 }
