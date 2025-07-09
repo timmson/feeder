448826604b37e00588a4cb5c9da17d12f68eb4e3
timmson <timmson666@mail.ru>
2025-07-09 20:32:37 +0300

    Fix typo

--- feeder-web/src/main/kotlin/ru/timmson/feeder/service/FeederFacade.kt
+++ feeder-web/src/main/kotlin/ru/timmson/feeder/service/FeederFacade.kt
@@ -92,7 +92,7 @@ class FeederFacade(
 
         val cvFile = botService.downloadFile(cvRequest.fileId)
         val estimation = cvEstimationService.estimate(cv.title.lowercase(), cvFile)
-        botService.sendMessage(cvRequest.chatId, "Оценка кандидата ${cv.name} на позиицию ${cv.title.lowercase()}:\n\n<i>$estimation</i>")
+        botService.sendMessage(cvRequest.chatId, "Оценка кандидата ${cv.name} на позицию ${cv.title.lowercase()}:\n\n<i>$estimation</i>")
 
         log.info("Leaving registerCV(...) = $cv")
     }
