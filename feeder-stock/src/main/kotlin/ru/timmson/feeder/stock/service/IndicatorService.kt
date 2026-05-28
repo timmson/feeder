d20d18e109caaad79111776de419e76f0bdc5ad4
Artem Krotov <timmson666@mail.ru>
2026-05-29 00:01:56 +0300

    Fix cache (#63)

--- feeder-stock/src/main/kotlin/ru/timmson/feeder/stock/service/IndicatorService.kt
+++ feeder-stock/src/main/kotlin/ru/timmson/feeder/stock/service/IndicatorService.kt
@@ -4,17 +4,16 @@ import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.runBlocking
-import org.springframework.cache.annotation.CacheEvict
 import org.springframework.stereotype.Service
 import ru.timmson.feeder.common.logger
 import ru.timmson.feeder.stock.dao.*
 import ru.timmson.feeder.stock.model.Indicator
 
 @Service
-open class IndicatorService(
-    currencyRateDAO: CurrencyRateDAO,
-    moscowExchangeDAO: MoscowExchangeDAO,
-    mainInfoDAO: MainInfoDAO,
+class IndicatorService(
+    private val currencyRateDAO: CurrencyRateDAO,
+    private val moscowExchangeDAO: MoscowExchangeDAO,
+    private val mainInfoDAO: MainInfoDAO,
     private val stockStorageDAO: StockStorageDAO,
     private val stockFileStorageService: StockFileStorageService
 ) {
@@ -30,11 +29,6 @@ open class IndicatorService(
 
     fun findAll(): List<Indicator> = runBlocking {
         daos.entries.asFlow().transform { emit(getStocks(it)) }.toList().flatten()
-    }
-
-    @CacheEvict(cacheNames = ["tickers", "mainInfo", "cursInfo"], allEntries = true)
-    open fun refreshAll() {
-        log.info("Cache cleared, refreshing all stock data")
     }
 
     fun put(stock: Indicator) = stockFileStorageService.setStock(stock)
